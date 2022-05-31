c74fd1aaaf144868cb3c73f0dc9837ce95347b4d
Cividati <rubens.cividati.teles@gmail.com>
2022-05-30 22:28:17 -0300

    feat: add route /bubblesort

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template, request                     
+from flask import Flask, render_template, request        
+import algorithms as algorithms
+
 app = Flask(__name__,template_folder="template")          
                                                         
 @app.route("/")
 def hello():
     return 'Hello world' 
 
+@app.route("/bubblesort")
+def bubblesort():
+      array = request.args.get('array')
+  
+      new_array = algorithms.Sorting.bubbleSort(array.split(','))
+
+      response = f"Your array: {array.split(',')} Bubble sort: {new_array}"
+ 
+      return response
 
 if __name__ == "__main__":
-      app.run(debug=True ,port=8080,use_reloader=True) 
+      app.run(debug=True ,port=80,use_reloader=True) 
